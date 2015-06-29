af04093d43566be7bfd1ccbbdc02f2b3c340fb43
Erik Andersen <erik.andersen@parivedasolutions.com>
2015-06-29 06:57:03 -0500

    Added iOS Resources Basic setup for reusable table source (iOS)

---
+++ MVVM Skeleton/Shared.VM/Interfaces/IListItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Shared.VM
+{
+	public enum RowItemType
+	{
+		Header,
+		Default,
+		Timeline
+	}
+	public interface IRowItem
+	{
+		Action<IRowItem> OnSelected { get; set; }
+		RowItemType ListItemType { get; set; }
+
+		void Selected ();
+	}
+}
+
