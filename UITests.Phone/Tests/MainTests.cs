7178cb6bd9dcea5a1387bb32e578912bf00d128d
Erik Andersen <erik.andersen@parivedasolutions.com>
2015-09-21 07:06:21 -0500

    Fixed UITests to follow the correct paradigm of screenshots

--- UITests.Phone/Tests/MainTests.cs
+++ UITests.Phone/Tests/MainTests.cs
@@ -39,168 +39,113 @@ namespace UITests.Phone
         private void SignInToFacebook ()
         {
             // Sign in to Facebook
-            try
-            {
-                App.Screenshot("Presenting Facebook Login");
-                App.Tap(c => c.Marked("Facebook"));
+            App.Tap(c => c.Marked("Facebook"));
+            App.Screenshot("Presenting Facebook Login");
 
-                App.Screenshot("Entering Facebook Username");
-                App.Tap(c => c.WebView().Css("[name=\"email\"]"));
-                App.EnterText("hashtaggapp@gmail.com");
+            App.Tap(c => c.WebView().Css("[name=\"email\"]"));
+            App.EnterText("hashtaggapp@gmail.com");
+            App.Screenshot("Entering Facebook Username");
 
-                App.Screenshot("Entering Facebook Password");
-                App.Tap(c => c.WebView().Css("[name=\"pass\"]")); 
-                App.EnterText("Hashtaggapp123$");
+            App.Tap(c => c.WebView().Css("[name=\"pass\"]")); 
+            App.EnterText("Hashtaggapp123$");
+            App.Screenshot("Entering Facebook Password");
 
-                App.Screenshot("Authorizing Facebook");
-                App.Tap(c => c.WebView().Css("[name=\"login\"]"));    
-            }
-            catch (Exception)
-            {
-                App.Screenshot("Failed to Sign in to Facebook");
-                throw;
-            }
+            App.Tap(c => c.WebView().Css("[name=\"login\"]"));    
+            App.Screenshot("Authorizing Facebook");
         }
 
         private void SignInToTwitter ()
         {
             // Sign in to Twitter
-            try
-            {
-                App.Screenshot("Presenting Twitter Login");
-                App.Tap(c => c.Marked("Twitter"));
+            App.Tap(c => c.Marked("Twitter"));
+            App.Screenshot("Presenting Twitter Login");
 
-                App.Screenshot("Entering Twitter Username");
-                App.Tap(c => c.WebView().Css("[name=\"session[username_or_email]\"]"));
-                App.EnterText("HashtaggApp");
+            App.Tap(c => c.WebView().Css("[name=\"session[username_or_email]\"]"));
+            App.EnterText("HashtaggApp");
+            App.Screenshot("Entering Twitter Username");
 
-                App.Screenshot("Entering Twitter Password");
-                App.Tap(c => c.WebView().Css("[name=\"session[password]\"]")); 
-                App.EnterText("Hashtagg123$");
+            App.Tap(c => c.WebView().Css("[name=\"session[password]\"]")); 
+            App.EnterText("Hashtagg123$");
+            App.Screenshot("Entering Twitter Password");
 
-                App.Screenshot("Authorizing Twitter");
-                App.Tap(c => c.WebView().Css("[id=\\\"allow\\\"]")); 
-            }
-            catch (Exception)
-            {
-                App.Screenshot("Failed to Sign in to Twitter");
-                throw;
-            }
+            App.Tap(c => c.WebView().Css("[id=\\\"allow\\\"]")); 
+            App.Screenshot("Authorizing Twitter");
         }
 
         private void SignIn ()
         {
-            try
-            {
-                App.Screenshot("Signing in with all accounts authorized");
-                App.WaitForElement(c => c.Marked("Let's go!"));
-                App.Tap(c => c.Marked("Let's go!"));
+            App.WaitForElement(c => c.Marked("Let's go!"));
+            App.Tap(c => c.Marked("Let's go!"));
+            App.Screenshot("Signing in with all accounts authorized");
 
-                // Dismiss alert if exists
-                Thread.Sleep(3000);
-                if(App.Query(c => c.Marked("OK")).Length > 0)
-                {
-                    App.Screenshot("Dismiss alert");
-                    App.Tap(c => c.Marked("OK"));
-                }   
-            }
-            catch (Exception)
+            // Dismiss alert if exists
+            Thread.Sleep(3000);
+            if(App.Query(c => c.Marked("OK")).Length > 0)
             {
-                App.Screenshot("Failed to Sign in");
-                throw;
-            }
+                App.Tap(c => c.Marked("OK"));
+            }   
         }
 
         private void OpenMenu ()
         {
-            try
-            {
-                App.Screenshot("Opening Menu");
-                App.WaitForElement(c => c.Marked("Menu Button"));
-                App.Tap(c => c.Marked("Menu Button"));
+            App.WaitForElement(c => c.Marked("Menu Button"));
+            App.Tap(c => c.Marked("Menu Button"));
+            App.Screenshot("Opening Menu");
 
-                App.Screenshot("Tapping Sign out");
-                App.WaitForElement(c => c.Marked("Sign out"));
-                App.Tap(c => c.Marked("Sign out"));
+            App.WaitForElement(c => c.Marked("Sign out"));
+            App.Tap(c => c.Marked("Sign out"));
+            App.Screenshot("Tapping Sign out");
 
-                App.Screenshot("Tapping Done");
-                App.WaitForElement(c => c.Marked("Done!"));
-                App.Tap(c => c.Marked("Done!"));
-            }
-            catch (Exception)
-            {
-                App.Screenshot("Failed to open menu");
-                throw; 
-            }
+            App.WaitForElement(c => c.Marked("Done!"));
+            App.Tap(c => c.Marked("Done!"));
+            App.Screenshot("Tapping Done");
         }
 
         private void CloseMenu ()
         {
-            try
-            {
-                App.Screenshot("Closing menu");
-                App.WaitForElement(c => c.Marked("Menu Button"));
-                App.Tap(c => c.Marked("Menu Button"));
-            }
-            catch (Exception)
-            {
-                App.Screenshot("Failed to close menu");
-                throw;
-            }
+            App.WaitForElement(c => c.Marked("Menu Button"));
+            App.Tap(c => c.Marked("Menu Button"));
+            App.Screenshot("Closing menu");
         }
 
         private void LikeUnlikePost ()
         {
-            try
+            if(App.Query(c => c.Property("text").Like("*Like (*")).Length < 0)
             {
-                App.Screenshot("Like Post");
-
-                if(App.Query(c => c.Property("text").Like("*Like (*")).Length < 0)
-                {
-                    App.ScrollDown();   
-                }
-                App.Tap(c => c.Property("text").Like("*Like (*"));
-
-                App.Screenshot("Unlike Post");
-                App.WaitForElement(c => c.Property("text").Like("*Liked (*"));
-                App.Tap(c => c.Property("text").Like("*Liked (*"));
+                App.ScrollDown();   
             }
-            catch (Exception)
-            {
-                App.Screenshot("Failed to like/unlike post");
-                throw; 
-            }
+            App.Tap(c => c.Property("text").Like("*Like (*"));
+            App.Screenshot("Like Post");
+
+            App.WaitForElement(c => c.Property("text").Like("*Liked (*"));
+            App.Tap(c => c.Property("text").Like("*Liked (*"));
+            App.Screenshot("Unlike Post");
         }
 
         private void NewPost ()
         {
-            try
-            {
-                App.Screenshot("Opening New Post");
-                App.WaitForElement(c => c.Marked("newPostButton"));
-                App.Tap(c => c.Marked("newPostButton"));
+            App.WaitForElement(c => c.Marked("newPostButton"));
+            App.Tap(c => c.Marked("newPostButton"));
+            App.Screenshot("Opening New Post");
 
-                App.Screenshot("Entering text");
-                App.WaitForElement("New Post");
+            App.WaitForElement("New Post");
 
-                Thread.Sleep(2000);
-                App.EnterText("This is a test post. Please ignore.");
+            Thread.Sleep(2000);
+            App.EnterText("This is a test post. Please ignore.");
+            App.Screenshot("Entering text");
 
-                App.Screenshot("Select social accounts");
-                App.Tap("socialTwitterUnselected");
+            App.Tap("socialTwitterUnselected");
+            App.Screenshot("Select Twitter account");
 
-                App.Screenshot("Post");
-                App.Tap("Post");
+            App.Tap("socialFacebookUnselected");
+            App.Screenshot("Select Facebook account");
 
-                App.Screenshot("Dismiss alert");
-                App.WaitForElement("OK");
-                App.Tap("OK");
-            }
-            catch (Exception)
-            {
-                App.Screenshot("Failed to create post");
-                throw; 
-            }
+            App.Tap("Post");
+            App.Screenshot("Post");
+
+            App.WaitForElement("OK");
+            App.Tap("OK");
+            App.Screenshot("Dismiss alert");
         }
     }
 }
